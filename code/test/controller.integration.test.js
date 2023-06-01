ee170d3375a28c73fb32d0a1183a5da5ba792a77
NICOLA GUTIERREZ s317290 <s317290@studenti.polito.it>
2023-06-01 17:32:21 +0200

    feat: adding deleteCategory base test (unstable) and more cases for updateCategory test

--- code/test/controller.integration.test.js
+++ code/test/controller.integration.test.js
@@ -7,20 +7,25 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 beforeAll(async () => {
-  const dbName = "testingDatabaseController";
-  const url = `${process.env.MONGO_URI}/${dbName}`;
+    const dbName = "testingDatabaseController";
+    const url = `${process.env.MONGO_URI}/${dbName}`;
 
-  await mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
+    await mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
 });
 
 afterAll(async () => {
   await mongoose.connection.db.dropDatabase();
   await mongoose.connection.close();
 });
+
+const exampleAdminAccToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImV6d2FsbGV0QHRlc3QuY29tIiwiaWQiOiI2NDc2ZTAwMTNlZGQxZTQ4MzEwOGVhOGEiLCJ1c2VybmFtZSI6ImV6d2FsbGV0XzMxXzA1XzIwMjQiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE2ODU1MTIyMTIsImV4cCI6MTcxNzA0ODIxMn0.WjFDAfn9X9hkLFt-6sx8T6cGMsRnSYIdw27mERvRelQ";
+const exampleAdminRefToken=exampleAdminAccToken;
+
+const exampleUserAccToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QzNjVAdGVzdC5jb20iLCJpZCI6IjY0NzZlMDhkM2VkZDFlNDgzMTA4ZWE5MCIsInVzZXJuYW1lIjoidGVzdF8zMV8wNV8yMDI0Iiwicm9sZSI6IlJlZ3VsYXIiLCJpYXQiOjE2ODU1MTIzNDUsImV4cCI6MTcxNzA0ODM0NX0._1pvR1CW1qSuIj_XnM2aKZWD7dC7ToACiXkvxsahRkk";
+const exampleUserRefToken=exampleUserAccToken;
 
 describe("createCategory", () => { 
     test('Dummy test, change it', () => {
@@ -29,15 +34,6 @@ describe("createCategory", () => {
 })
 
 describe("updateCategory", () => { 
-    const exampleAdminAccToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImV6d2FsbGV0QHRlc3QuY29tIiwiaWQiOiI2NDc2ZTAwMTNlZGQxZTQ4MzEwOGVhOGEiLCJ1c2VybmFtZSI6ImV6d2FsbGV0XzMxXzA1XzIwMjQiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE2ODU1MTIyMTIsImV4cCI6MTcxNzA0ODIxMn0.WjFDAfn9X9hkLFt-6sx8T6cGMsRnSYIdw27mERvRelQ";
-    const exampleAdminRefToken=exampleAdminAccToken;
-
-    const exampleUserAccToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QzNjVAdGVzdC5jb20iLCJpZCI6IjY0NzZlMDhkM2VkZDFlNDgzMTA4ZWE5MCIsInVzZXJuYW1lIjoidGVzdF8zMV8wNV8yMDI0Iiwicm9sZSI6IlJlZ3VsYXIiLCJpYXQiOjE2ODU1MTIzNDUsImV4cCI6MTcxNzA0ODM0NX0._1pvR1CW1qSuIj_XnM2aKZWD7dC7ToACiXkvxsahRkk";
-    const exampleUserRefToken=exampleUserAccToken;
-
-    /**
-    * Database is cleared before each test case, in order to allow insertion of data tailored for each specific test case.
-    */
     beforeEach(async () => {
         await categories.deleteMany({})
         await transactions.deleteMany({})
@@ -58,7 +54,7 @@ describe("updateCategory", () => {
         request(app)
             .patch("/api/categories/category1")
             .set("Cookie", `accessToken=${exampleAdminAccToken};refreshToken=${exampleAdminRefToken}`)
-            .send({newType: "category2", newColor: "yellow" }) // TODO: edit in order to handle requirements
+            .send({type: "category2", color: "yellow" })
             .then((response) => {
                 expect(response.status).toBe(200)
                 expect(response.body.data.message).toEqual("Category edited successfully")
--- code/test/controller.unit.test.js
+++ code/test/controller.unit.test.js
@@ -3,7 +3,7 @@ import { app } from '../app';
 import { categories, transactions } from "../models/model";
 import { Group, User, UserSchema } from "../models/User.js";
 import { handleDateFilterParams, handleAmountFilterParams, verifyAuth } from "../controllers/utils.js";
-import { updateCategory } from '../controllers/controller.js';
+import { updateCategory, deleteCategory } from '../controllers/controller.js';
 
 jest.mock("../models/model");
 jest.mock("../models/User.js");
@@ -14,6 +14,7 @@ beforeEach(() => {
     categories.findOne.mockClear();
     categories.updateOne.mockClear();
     categories.updateMany.mockClear();
+    categories.deleteMany.mockClear();
     categories.prototype.save.mockClear();
     transactions.find.mockClear();
     transactions.deleteOne.mockClear();
@@ -21,19 +22,19 @@ beforeEach(() => {
     transactions.prototype.save.mockClear();
 });
 
+const exampleAdminAccToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImV6d2FsbGV0QHRlc3QuY29tIiwiaWQiOiI2NDc2ZTAwMTNlZGQxZTQ4MzEwOGVhOGEiLCJ1c2VybmFtZSI6ImV6d2FsbGV0XzMxXzA1XzIwMjQiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE2ODU1MTIyMTIsImV4cCI6MTcxNzA0ODIxMn0.WjFDAfn9X9hkLFt-6sx8T6cGMsRnSYIdw27mERvRelQ";
+const exampleAdminRefToken=exampleAdminAccToken;
+
+const exampleUserAccToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QzNjVAdGVzdC5jb20iLCJpZCI6IjY0NzZlMDhkM2VkZDFlNDgzMTA4ZWE5MCIsInVzZXJuYW1lIjoidGVzdF8zMV8wNV8yMDI0Iiwicm9sZSI6IlJlZ3VsYXIiLCJpYXQiOjE2ODU1MTIzNDUsImV4cCI6MTcxNzA0ODM0NX0._1pvR1CW1qSuIj_XnM2aKZWD7dC7ToACiXkvxsahRkk";
+const exampleUserRefToken=exampleUserAccToken;
+
 describe("createCategory", () => { 
     test('Dummy test, change it', () => {
         expect(true).toBe(true);
     });
 })
 
-describe("updateCategory", () => {  //TODO: edit with the correct requirements
-    const exampleAdminAccToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImV6d2FsbGV0QHRlc3QuY29tIiwiaWQiOiI2NDc2ZTAwMTNlZGQxZTQ4MzEwOGVhOGEiLCJ1c2VybmFtZSI6ImV6d2FsbGV0XzMxXzA1XzIwMjQiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE2ODU1MTIyMTIsImV4cCI6MTcxNzA0ODIxMn0.WjFDAfn9X9hkLFt-6sx8T6cGMsRnSYIdw27mERvRelQ";
-    const exampleAdminRefToken=exampleAdminAccToken;
-
-    const exampleUserAccToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QzNjVAdGVzdC5jb20iLCJpZCI6IjY0NzZlMDhkM2VkZDFlNDgzMTA4ZWE5MCIsInVzZXJuYW1lIjoidGVzdF8zMV8wNV8yMDI0Iiwicm9sZSI6IlJlZ3VsYXIiLCJpYXQiOjE2ODU1MTIzNDUsImV4cCI6MTcxNzA0ODM0NX0._1pvR1CW1qSuIj_XnM2aKZWD7dC7ToACiXkvxsahRkk";
-    const exampleUserRefToken=exampleUserAccToken;
-
+describe("updateCategory", () => {
     test("Should return status code 200", async() => {
         const mockReq = {
             params: { type: "oldCategory" },
@@ -43,6 +44,9 @@ describe("updateCategory", () => {  //TODO: edit with the correct requirements
         const mockRes = {
           status: jest.fn().mockReturnThis(),
           json: jest.fn(),
+          locals: {
+            refreshedTokenMessage: "expired token"
+          }
         }
 
         verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
@@ -72,13 +76,14 @@ describe("updateCategory", () => {  //TODO: edit with the correct requirements
         expect(categories.updateMany).toHaveBeenCalled()
 
         expect(mockRes.status).toHaveBeenCalledWith(200)
-        expect(mockRes.json).toHaveBeenCalledWith({ message: "Category edited successfully", count: 2 }, {refreshedTokenMessage: res.locals.refreshedTokenMessage})
+        expect(mockRes.json).toHaveBeenCalledWith({data: {message: "Category edited successfully", count: updateManyResult.modifiedCount}, 
+                                                                            refreshedTokenMessage: mockRes.locals.refreshedTokenMessage});
     });
 
-    test("Should return status code 400, request body does not contain all the necessary attributes", async() => {
+    test("Should return status code 400: request body does not contain all the necessary attributes", async() => {
         const mockReq = {
             params: { type: "oldCategory" },
-            body: { newType: "", newColor: "yellow" }, // TODO: edit with the correct requirements
+            body: { color: "yellow" },
             cookies: { accessToken:exampleAdminAccToken, refreshToken:exampleAdminRefToken }
         }
         const mockRes = {
@@ -107,13 +112,207 @@ describe("updateCategory", () => {  //TODO: edit with the correct requirements
 
         await updateCategory(mockReq, mockRes)
 
+        expect(verifyAuth).toHaveBeenCalled()
+
+        expect(mockRes.status).toHaveBeenCalledWith(400)
+    });
+
+    test("Should return status code 400: at least one of the parameters in the request body is an empty string", async() => {
+        const mockReq = {
+            params: { type: "oldCategory" },
+            body: { type: "", color: "yellow" },
+            cookies: { accessToken:exampleAdminAccToken, refreshToken:exampleAdminRefToken }
+        }
+        const mockRes = {
+          status: jest.fn().mockReturnThis(),
+          json: jest.fn(),
+        }
+
+        verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
+
+        const alreadyExists = undefined;
+        jest.spyOn(categories, "findOne").mockResolvedValue(alreadyExists)
+
+        const oldCategory = {type: "oldCategory", color: "red"}
+        jest.spyOn(categories, "find").mockResolvedValue(oldCategory)
+
+        const updateOneResult = {
+            "acknowledged" : true,
+            "matchedCount" : 0,
+            "modifiedCount" : 0,
+            "upsertedId" : "aaa"
+        }
+        jest.spyOn(categories, "updateOne").mockResolvedValue(updateOneResult)
+
+        const updateManyResult = { "acknowledged" : true, "matchedCount" : 2, "modifiedCount" : 2 }
+        jest.spyOn(categories, "updateMany").mockResolvedValue(updateManyResult)
+
+        await updateCategory(mockReq, mockRes)
+
+        expect(verifyAuth).toHaveBeenCalled()
+
+        expect(mockRes.status).toHaveBeenCalledWith(400)
+    });
+
+    test("Should return status code 400: the type of category passed as a route parameter does not represent a category in the database", async() => {
+        const mockReq = {
+            params: { type: "oldCategory" },
+            body: { type: "nonPresentCategory", color: "yellow" },
+            cookies: { accessToken:exampleAdminAccToken, refreshToken:exampleAdminRefToken }
+        }
+        const mockRes = {
+          status: jest.fn().mockReturnThis(),
+          json: jest.fn(),
+        }
+
+        verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
+
+        const alreadyExists = undefined;
+        jest.spyOn(categories, "findOne").mockResolvedValue(alreadyExists)
+
+        const oldCategory = undefined;
+        jest.spyOn(categories, "find").mockResolvedValue(oldCategory)
+
+        const updateOneResult = {
+            "acknowledged" : true,
+            "matchedCount" : 0,
+            "modifiedCount" : 0,
+            "upsertedId" : "aaa"
+        }
+        jest.spyOn(categories, "updateOne").mockResolvedValue(updateOneResult)
+
+        const updateManyResult = { "acknowledged" : true, "matchedCount" : 2, "modifiedCount" : 2 }
+        jest.spyOn(categories, "updateMany").mockResolvedValue(updateManyResult)
+
+        await updateCategory(mockReq, mockRes)
+
+        expect(verifyAuth).toHaveBeenCalled()
+        expect(categories.findOne).toHaveBeenCalled()
+        expect(categories.find).toHaveBeenCalled()
+
+        expect(mockRes.status).toHaveBeenCalledWith(400)
+    });
+
+    test("Should return status code 400: the type of category passed in the request body as the new type represents an already existing category in the database and that category is not the same as the requested one", async() => {
+        const mockReq = {
+            params: { type: "oldCategory" },
+            body: { type: "nonPresentCategory", color: "yellow" },
+            cookies: { accessToken:exampleAdminAccToken, refreshToken:exampleAdminRefToken }
+        }
+        const mockRes = {
+          status: jest.fn().mockReturnThis(),
+          json: jest.fn(),
+        }
+
+        verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
+
+        const alreadyExists = {type: "oldCategory", color: "red"};
+        jest.spyOn(categories, "findOne").mockResolvedValue(alreadyExists)
+
+        const oldCategory = undefined;
+        jest.spyOn(categories, "find").mockResolvedValue(oldCategory)
+
+        const updateOneResult = {
+            "acknowledged" : true,
+            "matchedCount" : 0,
+            "modifiedCount" : 0,
+            "upsertedId" : "aaa"
+        }
+        jest.spyOn(categories, "updateOne").mockResolvedValue(updateOneResult)
+
+        const updateManyResult = { "acknowledged" : true, "matchedCount" : 2, "modifiedCount" : 2 }
+        jest.spyOn(categories, "updateMany").mockResolvedValue(updateManyResult)
+
+        await updateCategory(mockReq, mockRes)
+
+        expect(verifyAuth).toHaveBeenCalled()
+        expect(categories.findOne).toHaveBeenCalled()
+
+        expect(mockRes.status).toHaveBeenCalledWith(400)
+    });
+
+    test("Should return status code 401: called by an authenticated user who is not an admin (authType = Admin)", async() => {
+        const mockReq = {
+            params: { type: "oldCategory" },
+            body: { type: "nonPresentCategory", color: "yellow" },
+            cookies: { accessToken:exampleUserAccToken, refreshToken:exampleUserRefToken }
+        }
+        const mockRes = {
+          status: jest.fn().mockReturnThis(),
+          json: jest.fn(),
+        }
+
+        verifyAuth.mockReturnValue({ authorized: false, cause: "Requested auth for a different role" })
+
+        const alreadyExists = {type: "oldCategory", color: "red"};
+        jest.spyOn(categories, "findOne").mockResolvedValue(alreadyExists)
+
+        const oldCategory = undefined;
+        jest.spyOn(categories, "find").mockResolvedValue(oldCategory)
+
+        const updateOneResult = {
+            "acknowledged" : true,
+            "matchedCount" : 0,
+            "modifiedCount" : 0,
+            "upsertedId" : "aaa"
+        }
+        jest.spyOn(categories, "updateOne").mockResolvedValue(updateOneResult)
+
+        const updateManyResult = { "acknowledged" : true, "matchedCount" : 2, "modifiedCount" : 2 }
+        jest.spyOn(categories, "updateMany").mockResolvedValue(updateManyResult)
+
+        await updateCategory(mockReq, mockRes)
+
+        expect(verifyAuth).toHaveBeenCalled()
+
         expect(mockRes.status).toHaveBeenCalledWith(401)
     });
 })
 
 describe("deleteCategory", () => { 
-    test('Dummy test, change it', () => {
-        expect(true).toBe(true);
+    const listOfCategories = [{_id: 0, type: "category1", color: "red"}, 
+                              {_id: 1, type: "category2", color: "red"}, 
+                              {_id: 2, type: "category3", color: "red"},
+                              {_id: 3, type: "category4", color: "red"},
+                              {_id: 4, type: "category5", color: "red"}];
+    const foundCategories = [{_id: 0, type: "category1", color: "red"}, {_id: 1, type: "category2", color: "red"}];
+
+    test("Should return status code 200", async() => {
+        const mockReq = {
+            body: { types: ["category1", "category2"] },
+            cookies: { accessToken:exampleAdminAccToken, refreshToken:exampleAdminRefToken }
+        }
+        const mockRes = {
+          status: jest.fn().mockReturnThis(),
+          json: jest.fn(),
+          locals: {
+            refreshedTokenMessage: "expired token"
+          }
+        }
+
+        verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
+
+        jest.spyOn(categories, "find")
+                                    .mockResolvedValueOnce(listOfCategories)
+                                    .mockResolvedValueOnce(listOfCategories)
+                                    .mockResolvedValueOnce(foundCategories)
+
+        const deleteManyResult = { "acknowledged" : true, "deletedCount" : 2 }
+        jest.spyOn(categories, "deleteMany").mockResolvedValue(deleteManyResult)
+
+        const updateManyResult = { "acknowledged" : true, "matchedCount" : 3, "modifiedCount" : 3 }
+        jest.spyOn(categories, "updateMany").mockResolvedValue(updateManyResult)
+
+        await deleteCategory(mockReq, mockRes)
+
+        expect(verifyAuth).toHaveBeenCalled()
+        //expect(categories.find).toHaveBeenCalled()
+        //expect(categories.deleteMany).toHaveBeenCalled()
+        //expect(categories.updateMany).toHaveBeenCalled()
+
+        expect(mockRes.status).toHaveBeenCalledWith(200)
+        expect(mockRes.json).toHaveBeenCalledWith({data: {message: "Categories deleted", count: updateManyResult.modifiedCount}, 
+                                                                            refreshedTokenMessage: mockRes.locals.refreshedTokenMessage});
     });
 })
 
