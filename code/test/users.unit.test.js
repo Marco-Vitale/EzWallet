1d02f05343594ec6ce60e508ea01afe84536c1f9
FILIPPO GERBAUDO s272032 <s272032@studenti.polito.it>
2023-06-02 10:14:49 +0200

    fix(test.unit): getUsers fix

--- code/test/users.unit.test.js
+++ code/test/users.unit.test.js
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { app } from '../app';
 import { User } from '../models/User.js';
 import {getUsers, getUser} from '../controllers/users';
-
+import {verifyAuth} from '../controllers/utils';
 /**
  * In order to correctly mock the calls to external modules it is necessary to mock them using the following line.
  * Without this operation, it is not possible to replace the actual implementation of the external functions with the one
@@ -10,7 +10,7 @@ import {getUsers, getUser} from '../controllers/users';
  * `jest.mock()` must be called for every external module that is called in the functions under test.
  */
 jest.mock("../models/User.js")
-
+jest.mock("../controllers/utils.js");
 /**
  * Defines code to be executed before each test case is launched
  * In this case the mock implementation of `User.find()` is cleared, allowing the definition of a new mock implementation.
@@ -45,7 +45,51 @@ describe("getUsers", () => {
 })*/
 
 describe("getUsers", () => {
-  test("should return empty list if there are no users", async () => {
+  test("(status 200) should return empty list if there are no users", async () => {
+    const mockReq = {
+    }
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+      locals: {
+        refreshedTokenMessage: "expired token"
+      }
+    }
+    verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
+
+    jest.spyOn(User, "find").mockResolvedValue([])
+    await getUsers(mockReq, mockRes)
+    expect(User.find).toHaveBeenCalled()
+    expect(mockRes.status).toHaveBeenCalledWith(200)
+    expect(mockRes.json).toHaveBeenCalledWith({ data: [], refreshedTokenMessage: mockRes.locals.refreshedTokenMessage})
+  })
+
+  test("(status 200) should retrieve list of all users", async () => {
+    const mockReq = {}
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+      locals: {
+        refreshedTokenMessage: "expired token"
+      }
+    }
+
+    verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
+  
+    const retrievedUsers = [{username: "Mario", email: "mario.red@email.com", role: "Regular"}, {username: "Luigi", email: "luigi.red@email.com", role: "Regular"}, {username: "admin", email: "admin@email.com", role: "Regular"} ]
+    
+    jest.spyOn(User, "find").mockResolvedValue(retrievedUsers) //Quando viene chiamato find su User viene simulata la risposta di un valore dato ovvero il retrievedUsers
+    await getUsers(mockReq, mockRes)
+    
+    expect(User.find).toHaveBeenCalled()
+    expect(mockRes.status).toHaveBeenCalledWith(200)
+    expect(mockRes.json).toHaveBeenCalledWith({ data: retrievedUsers, refreshedTokenMessage: mockRes.locals.refreshedTokenMessage})
+  })
+})
+
+describe("getUser", () => { 
+
+test("should retrive himself and return status 200", async () => {
     const mockReq = {
     }
     const mockRes = {
@@ -62,22 +106,7 @@ describe("getUsers", () => {
     expect(mockRes.json).toHaveBeenCalledWith({ data: [], message: mockRes.locals.refreshedTokenMessage})
   })
 
-  test("should retrieve list of all users", async () => {
-    const mockReq = {}
-    const mockRes = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    }
-    const retrievedUsers = [{ username: 'test1', email: 'test1@example.com', password: 'hashedPassword1' }, { username: 'test2', email: 'test2@example.com', password: 'hashedPassword2' }]
-    jest.spyOn(User, "find").mockResolvedValue(retrievedUsers) //Quando viene chiamato find su User viene simulata la risposta di un valore dato ovvero il retrievedUsers
-    await getUsers(mockReq, mockRes)
-    expect(User.find).toHaveBeenCalled()
-    expect(mockRes.status).toHaveBeenCalledWith(200)
-    expect(mockRes.json).toHaveBeenCalledWith(retrievedUsers)
-  })
 })
-
-describe("getUser", () => { })
 
 describe("createGroup", () => { })
 
