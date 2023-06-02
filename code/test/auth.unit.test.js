a724537221dcebd6935644ee93df10436c30dda7
RAFFAELE VIOLA s309063 <s309063@studenti.polito.it>
2023-06-02 17:30:14 +0200

    test: registerAdmin unit tests completed

--- code/test/auth.unit.test.js
+++ code/test/auth.unit.test.js
@@ -2,10 +2,24 @@ import request from 'supertest';
 import { app } from '../app';
 import { User } from '../models/User.js';
 import jwt from 'jsonwebtoken';
+import { registerAdmin } from '../controllers/auth';
+import { verifyEmail } from '../controllers/utils';
 const bcrypt = require("bcryptjs")
 
 jest.mock("bcryptjs")
 jest.mock('../models/User.js');
+jest.mock("../controllers/utils.js");
+
+beforeEach(() => {
+    jest.clearAllMocks();
+    //additional `mockClear()` must be placed here
+  });
+  
+  jest.mock('../controllers/utils.js', () => ({
+    verifyAuth: jest.fn(),
+    verifyEmail: jest.fn(),
+  }))
+
 
 describe('register', () => { 
     test('Dummy test, change it', () => {
@@ -14,9 +28,163 @@ describe('register', () => {
 });
 
 describe("registerAdmin", () => { 
-    test('Dummy test, change it', () => {
-        expect(true).toBe(true);
+    test('status 200 when successful creates an admin', async () => {
+    const mockReq = {
+        body: {username: "admin", email: "admin@email.com", password: "securePass"}
+    }
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+      locals: {
+        refreshedTokenMessage: "expired token"
+      }
+    }
+
+    verifyEmail.mockImplementation((email) => {
+        return true;
+      });
+
+    const newUser = {
+        
+            username: "admin",
+            email: "admin@email.com",
+            password: "securePass",
+            role: "Admin"
+    }
+    jest.spyOn(User, "findOne").mockResolvedValue(0);
+    jest.spyOn(bcrypt, "hash").mockResolvedValue("securePass")
+    jest.spyOn(User, "create").mockResolvedValue(true);
+    await registerAdmin(mockReq, mockRes);
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+    expect(mockRes.json).toHaveBeenCalledWith({data: {message: "User added succesfully"}})
     });
+
+
+    test('status 400 req does not contain all inputs', async () => {
+        const mockReq = {
+            body: {username: "admin", password: "securePass"}
+        }
+        const mockRes = {
+          status: jest.fn().mockReturnThis(),
+          json: jest.fn(),
+          locals: {
+            refreshedTokenMessage: "expired token"
+          }
+        }
+    
+        verifyEmail.mockImplementation((email) => {
+            return true;
+          });
+    
+        const newUser = {
+            
+                username: "admin",
+                email: "admin@email.com",
+                password: "securePass",
+                role: "Admin"
+        }
+        jest.spyOn(User, "findOne").mockResolvedValue(0);
+        jest.spyOn(bcrypt, "hash").mockResolvedValue("securePass")
+        jest.spyOn(User, "create").mockResolvedValue(true);
+        await registerAdmin(mockReq, mockRes);
+        expect(mockRes.status).toHaveBeenCalledWith(400);
+        expect(mockRes.json).toHaveBeenCalledWith({error: "Missing parameters"})
+    });
+    
+    test('status 400 if req contains an empty param', async () => {
+            const mockReq = {
+                body: {username: "admin", email: "  ", password: "securePass"}
+            }
+            const mockRes = {
+              status: jest.fn().mockReturnThis(),
+              json: jest.fn(),
+              locals: {
+                refreshedTokenMessage: "expired token"
+              }
+            }
+        
+            verifyEmail.mockImplementation((email) => {
+                return true;
+              });
+        
+            const newUser = {
+                
+                    username: "admin",
+                    email: "admin@email.com",
+                    password: "securePass",
+                    role: "Admin"
+            }
+            jest.spyOn(User, "findOne").mockResolvedValue(0);
+            jest.spyOn(bcrypt, "hash").mockResolvedValue("securePass")
+            jest.spyOn(User, "create").mockResolvedValue(true);
+            await registerAdmin(mockReq, mockRes);
+            expect(mockRes.status).toHaveBeenCalledWith(400);
+            expect(mockRes.json).toHaveBeenCalledWith({error: "Missing parameters"})
+    });
+    
+
+    test('status 400 if email is not valid', async () => {
+        const mockReq = {
+            body: {username: "admin", email: "prova.it", password: "securePass"}
+        }
+        const mockRes = {
+          status: jest.fn().mockReturnThis(),
+          json: jest.fn(),
+          locals: {
+            refreshedTokenMessage: "expired token"
+          }
+        }
+    
+        verifyEmail.mockImplementation((email) => {
+            return false;
+          });
+    
+        const newUser = {
+            
+                username: "admin",
+                email: "admin@email.com",
+                password: "securePass",
+                role: "Admin"
+        }
+        jest.spyOn(User, "findOne").mockResolvedValue(0);
+        jest.spyOn(bcrypt, "hash").mockResolvedValue("securePass")
+        jest.spyOn(User, "create").mockResolvedValue(true);
+        await registerAdmin(mockReq, mockRes);
+        expect(mockRes.status).toHaveBeenCalledWith(400);
+        expect(mockRes.json).toHaveBeenCalledWith({ error: "The email format is not valid!" })
+});
+
+test('status 400 if there is an already existing user', async () => {
+    const mockReq = {
+        body: {username: "admin", email: "prova@gmail.it", password: "securePass"}
+    }
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+      locals: {
+        refreshedTokenMessage: "expired token"
+      }
+    }
+
+    verifyEmail.mockImplementation((email) => {
+        return true;
+      });
+
+    const newUser = {
+        
+            username: "admin",
+            email: "admin@email.com",
+            password: "securePass",
+            role: "Admin"
+    }
+    jest.spyOn(User, "findOne").mockResolvedValue(newUser);
+    jest.spyOn(bcrypt, "hash").mockResolvedValue("securePass")
+    jest.spyOn(User, "create").mockResolvedValue(true);
+    await registerAdmin(mockReq, mockRes);
+    expect(mockRes.status).toHaveBeenCalledWith(400);
+    expect(mockRes.json).toHaveBeenCalledWith({ error: "The mail is already used!" })
+});
+        
 })
 
 describe('login', () => { 
