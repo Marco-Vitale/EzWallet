d951c0d9a8d053b717ce82804a08c08c8fef5958
Marco Vitale s312957 <s312957@studenti.polito.it>
2023-06-02 15:19:10 +0200

    feat: adding tests for createTransaction and createCategory, problem with date on unit test to be resolved (createTransaction)

--- code/test/controller.integration.test.js
+++ code/test/controller.integration.test.js
@@ -1,8 +1,10 @@
 import request from 'supertest';
 import { app } from '../app';
 import { categories, transactions } from '../models/model';
+import { User } from '../models/User';
 import mongoose, { Model } from 'mongoose';
 import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
 
 dotenv.config();
 
@@ -22,9 +24,82 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
+beforeEach(async () => {
+    await categories.deleteMany({})
+    await transactions.deleteMany({})
+    await User.deleteMany({})
+})
+
+const adminAccessTokenValid = jwt.sign({
+    email: "admin@email.com",
+    username: "admin",
+    role: "Admin"
+}, process.env.ACCESS_KEY, { expiresIn: '1y' })
+
+const testerAccessTokenValid = jwt.sign({
+    email: "tester@test.com",
+    username: "tester",
+    role: "Regular"
+}, process.env.ACCESS_KEY, { expiresIn: '1y' })
+
 describe("createCategory", () => { 
-    test('Dummy test, change it', () => {
-        expect(true).toBe(true);
+
+    test('Should return status code 200', async () => {
+        const response = await request(app)
+        .post("/api/categories")
+        .set("Cookie", `accessToken=${adminAccessTokenValid};refreshToken=${adminAccessTokenValid}`)
+        .send({type: "food", color: "blue"})
+
+        expect(response.status).toBe(200)
+        expect(response.body.data.type).toEqual("food")
+        expect(response.body.data.color).toEqual("blue")
+    });
+
+    test('Should return status code 400, missing parameters', async () => {
+        const response = await request(app)
+        .post("/api/categories")
+        .set("Cookie", `accessToken=${adminAccessTokenValid};refreshToken=${adminAccessTokenValid}`)
+        .send({type: "food"})
+
+        expect(response.status).toBe(400)
+        const errorMessage = response.body.error ? true : response.body.message ? true : false
+        expect(errorMessage).toBe(true)
+    });
+
+    test('Should return status code 400, parametes has an empty string', async () => {
+        const response = await request(app)
+        .post("/api/categories")
+        .set("Cookie", `accessToken=${adminAccessTokenValid};refreshToken=${adminAccessTokenValid}`)
+        .send({type: "food", color:""})
+
+        expect(response.status).toBe(400)
+        const errorMessage = response.body.error ? true : response.body.message ? true : false
+        expect(errorMessage).toBe(true)
+    });
+
+    test('Should return status code 400, already existing category', async () => {
+        await categories.create({ type: "food", color: "red" })
+
+        const response = await request(app)
+        .post("/api/categories")
+        .set("Cookie", `accessToken=${adminAccessTokenValid};refreshToken=${adminAccessTokenValid}`)
+        .send({type: "food", color:"blue"})
+
+        expect(response.status).toBe(400)
+        const errorMessage = response.body.error ? true : response.body.message ? true : false
+        expect(errorMessage).toBe(true)
+    });
+
+    test('Should return status code 401, the user is not an admin', async () => {
+
+        const response = await request(app)
+        .post("/api/categories")
+        .set("Cookie", `accessToken=${testerAccessTokenValid};refreshToken=${testerAccessTokenValid}`)
+        .send({type: "food", color:"blue"})
+
+        expect(response.status).toBe(401)
+        const errorMessage = response.body.error ? true : response.body.message ? true : false
+        expect(errorMessage).toBe(true)
     });
 })
 
@@ -47,9 +122,148 @@ describe("getCategories", () => {
 })
 
 describe("createTransaction", () => { 
-    test('Dummy test, change it', () => {
-        expect(true).toBe(true);
+    test('Should return status code 200', async () => {
+        await categories.create({ type: "food", color: "red" })
+        await User.create({username: "admin",
+        email: "admin@email.com",
+        password: "admin",
+        refreshToken: adminAccessTokenValid,
+        role: "Admin"})
+
+        const response = await request(app)
+        .post("/api/users/admin/transactions")
+        .set("Cookie", `accessToken=${adminAccessTokenValid};refreshToken=${adminAccessTokenValid}`)
+        .send({username: "admin", amount: 100, type: "food"})
+
+        expect(response.status).toBe(200)
+        expect(response.body).toHaveProperty("data")
     });
+
+    test('Should return status code 400, missing parameters', async () => {
+        await categories.create({ type: "food", color: "red" })
+        await User.create({username: "admin",
+        email: "admin@email.com",
+        password: "admin",
+        refreshToken: adminAccessTokenValid,
+        role: "Admin"})
+
+        const response = await request(app)
+        .post("/api/users/admin/transactions")
+        .set("Cookie", `accessToken=${adminAccessTokenValid};refreshToken=${adminAccessTokenValid}`)
+        .send({username: "admin", type: "food"})
+
+        expect(response.status).toBe(400)
+        const errorMessage = response.body.error ? true : response.body.message ? true : false
+        expect(errorMessage).toBe(true)
+    });
+
+    test('Should return status code 400, empty parameter', async () => {
+        await categories.create({ type: "food", color: "red" })
+        await User.create({username: "admin",
+        email: "admin@email.com",
+        password: "admin",
+        refreshToken: adminAccessTokenValid,
+        role: "Admin"})
+
+        const response = await request(app)
+        .post("/api/users/admin/transactions")
+        .set("Cookie", `accessToken=${adminAccessTokenValid};refreshToken=${adminAccessTokenValid}`)
+        .send({username: "admin", amount: 100, type: " "})
+
+        expect(response.status).toBe(400)
+        const errorMessage = response.body.error ? true : response.body.message ? true : false
+        expect(errorMessage).toBe(true)
+    });
+
+    test('Should return status code 400, category does not exist', async () => {
+        await User.create({username: "admin",
+        email: "admin@email.com",
+        password: "admin",
+        refreshToken: adminAccessTokenValid,
+        role: "Admin"})
+
+        const response = await request(app)
+        .post("/api/users/admin/transactions")
+        .set("Cookie", `accessToken=${adminAccessTokenValid};refreshToken=${adminAccessTokenValid}`)
+        .send({username: "admin", amount: 100, type: "food"})
+
+        expect(response.status).toBe(400)
+        const errorMessage = response.body.error ? true : response.body.message ? true : false
+        expect(errorMessage).toBe(true)
+    });
+
+    //! If the user passed as a parameter is different from the one in the token this test will return 401
+    //! beacuse the verify auth is executed firstly
+
+    test('Should return status code 400, users in body and parameter mismatch', async () => {
+        await categories.create({ type: "food", color: "red" })
+        await User.create({username: "admin",
+        email: "admin@email.com",
+        password: "admin",
+        refreshToken: adminAccessTokenValid,
+        role: "Admin"})
+
+        const response = await request(app)
+        .post("/api/users/admin/transactions")
+        .set("Cookie", `accessToken=${adminAccessTokenValid};refreshToken=${adminAccessTokenValid}`)
+        .send({username: "test", amount: 100, type: "food"})
+
+        expect(response.status).toBe(400)
+        const errorMessage = response.body.error ? true : response.body.message ? true : false
+        expect(errorMessage).toBe(true)
+    });
+
+    //!Since body username and param username are checked one against each other this test is valid for both cases.
+
+    test('Should return status code 400, users not in the db', async () => {
+        await categories.create({ type: "food", color: "red" })
+
+        const response = await request(app)
+        .post("/api/users/admin/transactions")
+        .set("Cookie", `accessToken=${adminAccessTokenValid};refreshToken=${adminAccessTokenValid}`)
+        .send({username: "admin", amount: 100, type: "food"})
+
+        expect(response.status).toBe(400)
+        const errorMessage = response.body.error ? true : response.body.message ? true : false
+        expect(errorMessage).toBe(true)
+    });
+
+    test('Should return status code 400, amount cannot be parsed', async () => {
+        await categories.create({ type: "food", color: "red" })
+        await User.create({username: "admin",
+        email: "admin@email.com",
+        password: "admin",
+        refreshToken: adminAccessTokenValid,
+        role: "Admin"})
+
+        const response = await request(app)
+        .post("/api/users/admin/transactions")
+        .set("Cookie", `accessToken=${adminAccessTokenValid};refreshToken=${adminAccessTokenValid}`)
+        .send({username: "admin", amount: "ciao", type: "food"})
+
+        expect(response.status).toBe(400)
+        const errorMessage = response.body.error ? true : response.body.message ? true : false
+        expect(errorMessage).toBe(true)
+    });
+
+    test('Should return status code 401, called by different user then route param', async () => {
+        await categories.create({ type: "food", color: "red" })
+        await User.create({username: "admin",
+        email: "admin@email.com",
+        password: "admin",
+        refreshToken: adminAccessTokenValid,
+        role: "Admin"})
+
+        const response = await request(app)
+        .post("/api/users/test/transactions")
+        .set("Cookie", `accessToken=${adminAccessTokenValid};refreshToken=${adminAccessTokenValid}`)
+        .send({username: "test", amount: "ciao", type: "food"})
+
+        expect(response.status).toBe(401)
+        const errorMessage = response.body.error ? true : response.body.message ? true : false
+        expect(errorMessage).toBe(true)
+    });
+
 })
 
 describe("getAllTransactions", () => { 
@@ -64,30 +278,35 @@ describe("getTransactionsByUser", () => {
     });
 })
 
+//TODO
 describe("getTransactionsByUserByCategory", () => { 
     test('Dummy test, change it', () => {
         expect(true).toBe(true);
     });
 })
 
+//TODO
 describe("getTransactionsByGroup", () => { 
     test('Dummy test, change it', () => {
         expect(true).toBe(true);
     });
 })
 
+//TODO
 describe("getTransactionsByGroupByCategory", () => { 
     test('Dummy test, change it', () => {
         expect(true).toBe(true);
     });
 })
 
+//TODO
 describe("deleteTransaction", () => { 
     test('Dummy test, change it', () => {
         expect(true).toBe(true);
     });
 })
 
+//TODO
 describe("deleteTransactions", () => { 
     test('Dummy test, change it', () => {
         expect(true).toBe(true);
--- code/test/controller.unit.test.js
+++ code/test/controller.unit.test.js
@@ -1,8 +1,14 @@
 import request from 'supertest';
 import { app } from '../app';
 import { categories, transactions } from '../models/model';
+import { User } from '../models/User';
+import { verifyAuth } from '../controllers/utils';
+import { createCategory, createTransaction } from '../controllers/controller';
 
 jest.mock('../models/model');
+jest.mock("../models/User.js");
+jest.mock("../controllers/utils.js");
+
 
 beforeEach(() => {
   categories.find.mockClear();
@@ -11,12 +17,150 @@ beforeEach(() => {
   transactions.deleteOne.mockClear();
   transactions.aggregate.mockClear();
   transactions.prototype.save.mockClear();
+  jest.clearAllMocks();
 });
 
+const exampleAdminAccToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImV6d2FsbGV0QHRlc3QuY29tIiwiaWQiOiI2NDc2ZTAwMTNlZGQxZTQ4MzEwOGVhOGEiLCJ1c2VybmFtZSI6ImV6d2FsbGV0XzMxXzA1XzIwMjQiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE2ODU1MTIyMTIsImV4cCI6MTcxNzA0ODIxMn0.WjFDAfn9X9hkLFt-6sx8T6cGMsRnSYIdw27mERvRelQ";
+const exampleAdminRefToken=exampleAdminAccToken;
+
+const exampleUserAccToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QzNjVAdGVzdC5jb20iLCJpZCI6IjY0NzZlMDhkM2VkZDFlNDgzMTA4ZWE5MCIsInVzZXJuYW1lIjoidGVzdF8zMV8wNV8yMDI0Iiwicm9sZSI6IlJlZ3VsYXIiLCJpYXQiOjE2ODU1MTIzNDUsImV4cCI6MTcxNzA0ODM0NX0._1pvR1CW1qSuIj_XnM2aKZWD7dC7ToACiXkvxsahRkk";
+const exampleUserRefToken=exampleUserAccToken;
+
 describe("createCategory", () => { 
-    test('Dummy test, change it', () => {
-        expect(true).toBe(true);
+    test("Should return status code 200", async () => {
+
+        const mockReq = {
+            body: { type: "food", color: "red"},
+            cookies: { accessToken:exampleAdminAccToken, refreshToken:exampleAdminRefToken }
+        }
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+            locals: {
+              refreshedTokenMessage: ""
+            }
+        }
+
+        verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
+
+        //This call simulate the case in which there are no categories with this type and color
+        categories.findOne.mockResolvedValueOnce(null)
+
+        const newCat = { type: "food", color: "red"}
+
+        categories.prototype.save.mockResolvedValue(newCat)
+
+        await createCategory(mockReq,mockRes)
+
+        expect(verifyAuth).toHaveBeenCalled()
+        expect(categories.findOne).toHaveBeenCalled()
+        expect(mockRes.status).toHaveBeenCalledWith(200)
+        expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({
+            data: newCat
+        }))
     });
+
+
+    test("Should return status code 400, missing parameters", async () => {
+
+        const mockReq = {
+            body: { type: "food"},
+            cookies: { accessToken:exampleAdminAccToken, refreshToken:exampleAdminRefToken }
+        }
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+            locals: {
+              refreshedTokenMessage: ""
+            }
+        }
+
+        verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
+
+        await createCategory(mockReq,mockRes)
+
+        expect(verifyAuth).toHaveBeenCalled()
+        expect(mockRes.status).toHaveBeenCalledWith(400)
+        expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({error: expect.any(String)}))
+
+    });
+
+    test("Should return status code 400, a parameter has an empty string", async () => {
+
+        const mockReq = {
+            body: { type: "food", color: ""},
+            cookies: { accessToken:exampleAdminAccToken, refreshToken:exampleAdminRefToken }
+        }
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+            locals: {
+              refreshedTokenMessage: ""
+            }
+        }
+
+        verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
+
+        await createCategory(mockReq,mockRes)
+
+        expect(verifyAuth).toHaveBeenCalled()
+        expect(mockRes.status).toHaveBeenCalledWith(400)
+        expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({error: expect.any(String)}))
+
+    });
+
+    test("Should return status code 400, already existing category", async () => {
+
+        const mockReq = {
+            body: { type: "food", color: "red"},
+            cookies: { accessToken:exampleAdminAccToken, refreshToken:exampleAdminRefToken }
+        }
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+            locals: {
+              refreshedTokenMessage: ""
+            }
+        }
+
+        verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
+
+        const existCat = { type: "food", color: "red"};
+
+        categories.findOne.mockResolvedValueOnce(existCat)
+
+        await createCategory(mockReq,mockRes)
+
+        expect(verifyAuth).toHaveBeenCalled()
+        expect(categories.findOne).toHaveBeenCalled()
+        expect(mockRes.status).toHaveBeenCalledWith(400)
+        expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({error: expect.any(String)}))
+    });
+
+    test("Should return status code 401, the user is not an admin", async () => {
+
+        const mockReq = {
+            body: { type: "food", color: "red"},
+            cookies: { accessToken:exampleUserAccToken, refreshToken:exampleUserRefToken }
+        }
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+            locals: {
+              refreshedTokenMessage: ""
+            }
+        }
+
+        verifyAuth.mockReturnValue({ authorized: false, cause: "Unauthorized" })
+
+        await createCategory(mockReq,mockRes)
+
+        expect(verifyAuth).toHaveBeenCalled()
+        expect(mockRes.status).toHaveBeenCalledWith(401)
+        expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({error: expect.any(String)}))
+
+    });
+
 })
 
 describe("updateCategory", () => { 
@@ -38,8 +182,241 @@ describe("getCategories", () => {
 })
 
 describe("createTransaction", () => { 
-    test('Dummy test, change it', () => {
-        expect(true).toBe(true);
+    test("Should return status code 200", async () => {
+
+        const mockReq = {
+            params: {username: "Mario"},
+            body: {username: "Mario", amount: 100, type: "food"},
+            cookies: { accessToken:exampleAdminAccToken, refreshToken:exampleAdminRefToken }
+        }
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+            locals: {
+              refreshedTokenMessage: ""
+            }
+        }
+
+        verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
+
+        const mockCategory = {
+            type: "food",
+            color: "red"
+        }
+        categories.findOne.mockResolvedValueOnce(mockCategory)
+
+        const mockUser = {
+            username: "Mario",
+            email: "xxx@yyyy.it",
+            password: "xxxxxxx",
+            refreshToken: "xxxx",
+            role:"Regular"
+        }
+
+        User.findOne.mockResolvedValueOnce(mockUser)
+
+        const newTrans = { username: "Mario", type: "food", amount: 100, date: Date.now}
+
+        transactions.prototype.save.mockResolvedValue(newTrans)
+
+        await createTransaction(mockReq,mockRes)
+
+        expect(verifyAuth).toHaveBeenCalled()
+        expect(categories.findOne).toHaveBeenCalled()
+        expect(User.findOne).toHaveBeenCalled()
+        expect(mockRes.status).toHaveBeenCalledWith(200)
+        expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({
+            data: newTrans
+        }))
+    });
+
+    test("Should return status code 400, missing parameters", async () => {
+
+        const mockReq = {
+            params: {username: "Mario"},
+            body: {username: "Mario", type: "food"},
+            cookies: { accessToken:exampleAdminAccToken, refreshToken:exampleAdminRefToken }
+        }
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+            locals: {
+              refreshedTokenMessage: ""
+            }
+        }
+
+        verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
+
+        await createTransaction(mockReq,mockRes)
+
+        expect(verifyAuth).toHaveBeenCalled()
+        expect(mockRes.status).toHaveBeenCalledWith(400)
+        expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({error: expect.any(String)}))
+    });
+
+    test("Should return status code 400, empty string", async () => {
+
+        const mockReq = {
+            params: {username: "Mario"},
+            body: {username: "Mario", amount: 100, type: ""},
+            cookies: { accessToken:exampleAdminAccToken, refreshToken:exampleAdminRefToken }
+        }
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+            locals: {
+              refreshedTokenMessage: ""
+            }
+        }
+
+        verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
+
+        await createTransaction(mockReq,mockRes)
+
+        expect(verifyAuth).toHaveBeenCalled()
+        expect(mockRes.status).toHaveBeenCalledWith(400)
+        expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({error: expect.any(String)}))
+    });
+
+    test("Should return status code 400, category not present", async () => {
+
+        const mockReq = {
+            params: {username: "Mario"},
+            body: {username: "Mario", amount: 100, type: "food"},
+            cookies: { accessToken:exampleAdminAccToken, refreshToken:exampleAdminRefToken }
+        }
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+            locals: {
+              refreshedTokenMessage: ""
+            }
+        }
+
+        verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
+
+        categories.findOne.mockResolvedValueOnce(null)
+
+        await createTransaction(mockReq,mockRes)
+
+        expect(verifyAuth).toHaveBeenCalled()
+        expect(categories.findOne).toHaveBeenCalled()
+        expect(mockRes.status).toHaveBeenCalledWith(400)
+        expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({error: expect.any(String)}))
+    });
+
+    test("Should return status code 400, user in body mismatch user in params", async () => {
+
+        const mockReq = {
+            params: {username: "Mirco"},
+            body: {username: "Mario", amount: 100, type: "food"},
+            cookies: { accessToken:exampleAdminAccToken, refreshToken:exampleAdminRefToken }
+        }
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+            locals: {
+              refreshedTokenMessage: ""
+            }
+        }
+
+        verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
+
+        const mockCategory = {
+            type: "food",
+            color: "red"
+        }
+        categories.findOne.mockResolvedValueOnce(mockCategory)
+
+        await createTransaction(mockReq,mockRes)
+
+        expect(verifyAuth).toHaveBeenCalled()
+        expect(categories.findOne).toHaveBeenCalled()
+        expect(mockRes.status).toHaveBeenCalledWith(400)
+        expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({error: expect.any(String)}))
+    });
+
+    //!Since body username and param username are checked one against each other this test is valid for both cases.
+
+    test("Should return status code 400, user not in the db", async () => {
+
+        const mockReq = {
+            params: {username: "Mario"},
+            body: {username: "Mario", amount: 100, type: "food"},
+            cookies: { accessToken:exampleAdminAccToken, refreshToken:exampleAdminRefToken }
+        }
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+            locals: {
+              refreshedTokenMessage: ""
+            }
+        }
+
+        verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
+
+        const mockCategory = {
+            type: "food",
+            color: "red"
+        }
+        categories.findOne.mockResolvedValueOnce(mockCategory)
+        User.findOne.mockResolvedValueOnce(null)
+
+        await createTransaction(mockReq,mockRes)
+
+        expect(verifyAuth).toHaveBeenCalled()
+        expect(categories.findOne).toHaveBeenCalled()
+        expect(User.findOne).toHaveBeenCalled()
+        expect(mockRes.status).toHaveBeenCalledWith(400)
+        expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({error: expect.any(String)}))
+    });
+
+    test("Should return status code 400, amount cannot be parsed", async () => {
+
+        const mockReq = {
+            params: {username: "Mario"},
+            body: {username: "Mario", amount: "ciao", type: "food"},
+            cookies: { accessToken:exampleAdminAccToken, refreshToken:exampleAdminRefToken }
+        }
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+            locals: {
+              refreshedTokenMessage: ""
+            }
+        }
+
+        verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
+
+        await createTransaction(mockReq,mockRes)
+
+        expect(verifyAuth).toHaveBeenCalled()
+        expect(mockRes.status).toHaveBeenCalledWith(400)
+        expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({error: expect.any(String)}))
+    });
+
+    test("Should return status code 401, called by different user then route param", async () => {
+
+        const mockReq = {
+            params: {username: "Mario"},
+            body: {username: "Mario", amount: "ciao", type: "food"},
+            cookies: { accessToken:exampleAdminAccToken, refreshToken:exampleAdminRefToken }
+        }
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+            locals: {
+              refreshedTokenMessage: ""
+            }
+        }
+
+        verifyAuth.mockReturnValue({ authorized: false, cause: "Requested auth for a different user" })
+
+        await createTransaction(mockReq,mockRes)
+
+        expect(verifyAuth).toHaveBeenCalled()
+        expect(mockRes.status).toHaveBeenCalledWith(401)
+        expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({error: expect.any(String)}))
     });
 })
 
@@ -55,30 +432,35 @@ describe("getTransactionsByUser", () => {
     });
 })
 
+//TODO
 describe("getTransactionsByUserByCategory", () => { 
     test('Dummy test, change it', () => {
         expect(true).toBe(true);
     });
 })
 
+//TODO
 describe("getTransactionsByGroup", () => { 
     test('Dummy test, change it', () => {
         expect(true).toBe(true);
     });
 })
 
+//TODO
 describe("getTransactionsByGroupByCategory", () => { 
     test('Dummy test, change it', () => {
         expect(true).toBe(true);
     });
 })
 
+//TODO
 describe("deleteTransaction", () => { 
     test('Dummy test, change it', () => {
         expect(true).toBe(true);
     });
 })
 
+//TODO
 describe("deleteTransactions", () => { 
     test('Dummy test, change it', () => {
         expect(true).toBe(true);
