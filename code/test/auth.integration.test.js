b1771ee2af228a17e3f577da15e71045b0955320
NICOLA GUTIERREZ s317290 <s317290@studenti.polito.it>
2023-06-05 09:12:01 +0200

    feat(integration test): adding register integration test

--- code/test/auth.integration.test.js
+++ code/test/auth.integration.test.js
@@ -9,13 +9,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 beforeAll(async () => {
-  const dbName = "testingDatabaseAuth";
-  const url = `${process.env.MONGO_URI}/${dbName}`;
+    const dbName = "testingDatabaseAuth";
+    const url = `${process.env.MONGO_URI}/${dbName}`;
 
-  await mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
 });
 
@@ -24,7 +24,94 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
+beforeEach(async () => {
+  await User.deleteMany({})
+})
+
+const adminAccessTokenValid = jwt.sign({
+  email: "admin@email.com",
+  username: "admin",
+  role: "Admin"
+}, process.env.ACCESS_KEY, { expiresIn: '1y' })
+
+const testerAccessTokenValid = jwt.sign({
+  email: "tester@test.com",
+  username: "tester",
+  role: "Regular"
+}, process.env.ACCESS_KEY, { expiresIn: '1y' })
+
+const testerAccessTokenExpired = jwt.sign({
+  email: "tester@test.com",
+  username: "tester",
+  role: "Regular"
+}, process.env.ACCESS_KEY, { expiresIn: '0s' })
+const testerAccessTokenEmpty = jwt.sign({}, process.env.ACCESS_KEY, { expiresIn: "1y" })
+
 describe('register', () => {
+  test("Should return 200 and register the user", async() => {
+    const response = await request(app)
+        .post("/api/register")
+        .send({username: "tester1", email: "tester1@test.com", password: "secure_password"})
+    
+    expect(response.status).toBe(200)
+    expect(response.body.data).toHaveProperty("message")
+  });
+
+  test("Should return 400: the request body does not contain all the necessary attributes", async() => {
+    const response = await request(app)
+        .post("/api/register")
+        .send({username: "tester1", password: "secure_password"})
+    
+    expect(response.status).toBe(400)
+    const errorMessage = response.body.error ? true : response.body.message ? true : false
+        expect(errorMessage).toBe(true)
+  });
+
+  test("Should return 400: at least one of the parameters in the request body is an empty strings", async() => {
+    const response = await request(app)
+        .post("/api/register")
+        .send({username: "tester1", email: "tester1@test.com", password: ""})
+    
+    expect(response.status).toBe(400)
+    const errorMessage = response.body.error ? true : response.body.message ? true : false
+        expect(errorMessage).toBe(true)
+  });
+
+  test("Should return 400: the email in the request body is not in a valid email format", async() => {
+    const response = await request(app)
+        .post("/api/register")
+        .send({username: "tester1", email: "tester1_test.com", password: "secure_password"})
+    
+    expect(response.status).toBe(400)
+    const errorMessage = response.body.error ? true : response.body.message ? true : false
+        expect(errorMessage).toBe(true)
+  });
+
+  test("Should return 400: the username in the request body identifies an already existing use", async() => {
+    await User.create(
+      {username: "tester1", email: "tester@test.com", password: "tester", role: "Regular", refreshToken: testerAccessTokenValid}
+    )
+    const response = await request(app)
+        .post("/api/register")
+        .send({username: "tester1", email: "tester1@test.com", password: "secure_password"})
+    
+    expect(response.status).toBe(400)
+    const errorMessage = response.body.error ? true : response.body.message ? true : false
+        expect(errorMessage).toBe(true)
+  });
+
+  test("Should return 400: the email in the request body identifies an already existing user", async() => {
+    await User.create(
+      {username: "tester", email: "tester1@test.com", password: "tester", role: "Regular", refreshToken: testerAccessTokenValid}
+    )
+    const response = await request(app)
+        .post("/api/register")
+        .send({username: "tester1", email: "tester1@test.com", password: "secure_password"})
+    
+    expect(response.status).toBe(400)
+    const errorMessage = response.body.error ? true : response.body.message ? true : false
+        expect(errorMessage).toBe(true)
+  });
 });
 
 describe("registerAdmin", () => { 
