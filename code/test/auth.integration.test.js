3c09d27c1fdfe02bd7f611e72ce9033655562c2d
FILIPPO GERBAUDO s272032 <s272032@studenti.polito.it>
2023-06-06 23:09:30 +0200

    feat(auth): login ustable for 200 response

--- code/test/auth.integration.test.js
+++ code/test/auth.integration.test.js
@@ -34,9 +34,154 @@ describe("registerAdmin", () => {
 })
 
 describe('login', () => { 
-    test('Dummy test, change it', () => {
-        expect(true).toBe(true);
-    });
+    
+   /**
+   * Database is cleared before each test case, in order to allow insertion of data tailored for each specific test case.
+   */
+   beforeEach(async () => {
+    await User.deleteMany({})
+  })
+  /* [ADMIN]
+  username: administratortest
+  password: administratorpassword
+  mail: administrator@test.com
+  */
+  const exampleAdminRefToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluaXN0cmF0b3JAdGVzdC5jb20iLCJpZCI6IjY0Nzg1YTI1MzJhZDk2MGIwNWZhMmJlMiIsInVzZXJuYW1lIjoiYWRtaW5pc3RyYXRvcnRlc3QiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE2ODU2MDkwMTEsImV4cCI6MTY4NjIxMzgxMX0.GAlIbL2MjisoEjDE8kHJAjVkAzjUsJjYzmhj6lxi3Yo";
+  const exampleAdminAccToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluaXN0cmF0b3JAdGVzdC5jb20iLCJpZCI6IjY0Nzg1YTI1MzJhZDk2MGIwNWZhMmJlMiIsInVzZXJuYW1lIjoiYWRtaW5pc3RyYXRvcnRlc3QiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE2ODU2MDkwMTEsImV4cCI6MTY4NjIxMzgxMX0.GAlIbL2MjisoEjDE8kHJAjVkAzjUsJjYzmhj6lxi3Yo";
+
+  /* [REGULAR]
+  {
+    "username":"test", 
+    "email": "test@test.com",
+    "password": "tester"
+}
+  */
+  
+  
+
+  test.skip("[REGULAR](status: 200) should login regular user", (done) => {
+    User.create({
+      username: "tester",
+      email: "test@test.com",
+      password: "tester_pass",
+      role: "Regular",
+      
+    }).then((currUser) => {
+      request(app)
+        .post("/api/login")
+        .send({"email": "test@test.com", "password": "tester_pass"})
+        .then((response) => {
+          console.log(response.body)
+          expect(response.status).toBe(200)
+
+  
+          //Controlli sui campi
+          expect(response.body).toHaveProperty("data")
+          expect(response.body.data).toHaveProperty("accessToken")
+          expect(response.body.data).toHaveProperty("refreshToken")
+
+          const decodedAccessToken = jwt.verify(response.body.accessToken, process.env.ACCESS_KEY);
+
+          expect(decodedAccessToken.username).toEqual("tester")
+          expect(decodedAccessToken.email).toEqual("test@test.com")
+          expect(decodedAccessToken.role).toEqual("Regular")
+
+          const decodedRefreshToken = jwt.verify(response.body.refreshToken, process.env.ACCESS_KEY);
+
+          expect(decodedRefreshToken.username).toEqual("tester")
+          expect(decodedRefreshToken.email).toEqual("test@test.com")
+          expect(decodedRefreshToken.role).toEqual("Regular")
+        
+          expect(currUser.refreshToken).toEqual(response.body.refreshToken)
+
+
+          done() // Notify Jest that the test is complete
+        })
+        .catch((err) => done(err))
+    })
+  })
+
+  test("(status: 400) if the request body does not contain all the necessary attributes", (done) => {
+    User.create({
+      username: "tester",
+      email: "test@test.com",
+      password: "tester_pass",
+      role: "Regular",
+      
+    }).then((currUser) => {
+      request(app)
+        .post("/api/login")
+        .send({})
+        .then((response) => {
+          expect(response.status).toBe(400)
+          expect(response.body).toHaveProperty("error") 
+          done() // Notify Jest that the test is complete
+        })
+        .catch((err) => done(err))
+    })
+  })
+
+  test("(status: 400) if the email in the request body is not in a valid email format", (done) => {
+    User.create({
+      username: "tester",
+      email: "test@test.com",
+      password: "tester_pass",
+      role: "Regular",
+      
+    }).then((currUser) => {
+      request(app)
+        .post("/api/login")
+        .send({email: "tester", password:"tester_pass"})
+        .then((response) => {
+          expect(response.status).toBe(400)
+          expect(response.body).toHaveProperty("error") 
+          done() // Notify Jest that the test is complete
+        })
+        .catch((err) => done(err))
+    })
+  })
+
+  test("(status: 400) if the email in the request body does not identify a user in the database", (done) => {
+    User.create({
+      username: "tester",
+      email: "test@test.com",
+      password: "tester_pass",
+      role: "Regular",
+      
+    }).then((currUser) => {
+      request(app)
+        .post("/api/login")
+        .send({email: "gianni@test.com", password:"tester_pass"})
+        .then((response) => {
+          expect(response.status).toBe(400)
+          expect(response.body).toHaveProperty("error") 
+          done() // Notify Jest that the test is complete
+        })
+        .catch((err) => done(err))
+    })
+  })
+
+  test("(status: 400) if the supplied password does not match with the one in the database", (done) => {
+    User.create({
+      username: "tester",
+      email: "test@test.com",
+      password: "tester_pass",
+      role: "Regular",
+      
+    }).then((currUser) => {
+      request(app)
+        .post("/api/login")
+        .send({email: "test@test.com", password:"tester"})
+        .then((response) => {
+          expect(response.status).toBe(400)
+          console.log(response.body)
+          expect(response.body).toHaveProperty("error") 
+          done() // Notify Jest that the test is complete
+        })
+        .catch((err) => done(err))
+    })
+  })
+
 });
 
 describe('logout', () => { 
--- code/test/auth.unit.test.js
+++ code/test/auth.unit.test.js
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../app';
 import { User } from '../models/User.js';
+import { login } from '../controllers/auth.js'; 
 import jwt from 'jsonwebtoken';
 const bcrypt = require("bcryptjs")
 
@@ -19,11 +20,69 @@ describe("registerAdmin", () => {
     });
 })
 
-describe('login', () => { 
-    test('Dummy test, change it', () => {
-        expect(true).toBe(true);
-    });
+
+    
+
+describe('login', () => {
+  
+  test('should return 200 for correct login', async () => {
+    const req = {
+      body: {
+        email: 'test@test.com',
+        password: 'password123'
+      }
+    }
+    const res = { 
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()
+    }
+
+    User.findOne.mockResolvedValueOnce(undefined)
+
+    resp = await login(req, res)
+        
+    expect(res.status).toHaveBeenCalledWith(200)
+    expect(res.json).toHaveBeenCalledWith({
+      data: {
+        accessToken: expect.any(String),
+        refreshToken: expect.any(String)
+      }
+    })
+  });
+
+  test('should return 400 if the request body does not contain all the necessary attributes', async () => {
+    const req = { body: {} }
+    const res = { 
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()
+    }
+    await login(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({ error: 'Missing parameters' });
+  });
+
+  test('should return 400 if at least one of the parameters in the request body is an empty string', async () => {
+    const req = { body: { email: '', password: '' } }
+    const res = { 
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()}
+    await login(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({ error: 'Missing parameters' });
+  });
+
+  test('should return 400 if the email in the request body is not in a valid email format', async () => {
+    const req = { body: { email: 'invalidEmail', password: 'password123' } }
+    const res = { 
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()}
+    await login(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({ error: 'The email format is not valid!' });
 });
+});
+   
+
 
 describe('logout', () => { 
     test('Dummy test, change it', () => {
