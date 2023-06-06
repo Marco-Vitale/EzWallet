c9900ae95299ee2ce5b668d52145e6eb48104041
FILIPPO GERBAUDO s272032 <s272032@studenti.polito.it>
2023-06-06 18:47:58 +0200

    fix(user.unit): removeFromGroup addTo group minor fixes

--- code/test/users.unit.test.js
+++ code/test/users.unit.test.js
@@ -561,7 +561,7 @@ describe("removeFromGroup", () => {
   const exampleUserRefToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJpZCI6IjY0NzhhNTg2MzdlZmM0YWZmMmVlNWVlMyIsInVzZXJuYW1lIjoidGVzdGVyIiwicm9sZSI6IlJlZ3VsYXIiLCJpYXQiOjE2ODU2MzExMjIsImV4cCI6MTY4NjIzNTkyMn0.XhUwuKINOEQHtYE1hOCPv-a5TR_NIB4l-R9AdRnL024";
   const exampleUserAccToken= exampleUserRefToken;
 
-  test("(status 200)[REGULAR] should return all added user", async () => {
+  test("(status 200)[REGULAR] should return group without removed users", async () => {
     const mockReq = {
       params: { 
         name: "Family"},
@@ -579,21 +579,17 @@ describe("removeFromGroup", () => {
     
     //Acutal group
     const actualGroup = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"},{email: "luigi.red@email.com", _id: "647edb912de5a0e0e1702caf"},{email: "pietro.blue@email.com", _id:"647edba4b9ae4baa5e292dec"}]}
-    const actualGroup_1 = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"},{email: "pietro.blue@email.com", _id:"647edba4b9ae4baa5e292dec"}]}
-    const actualGroup_2 = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"}]}
+    
     //Final group with add of luigi and mario 
-    const resolvedValue = {group: {name: "Family", members: [{email: "test@test.com"}]}, membersNotFound: [], alreadyInGroup: []} 
+    const resolvedValue = {group: {name: "Family", members: [{email: "test@test.com"}]}, membersNotFound: [], notInGroup: []}
 
-    Group.findOne.mockResolvedValueOnce(actualGroup)
-    verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
+    Group.findOne.mockResolvedValueOnce(actualGroup) //OK
+    verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" }) //OK
     verifyEmail.mockReturnValue({ verified: true, cause: "Verified" })
     User.findOne.mockResolvedValueOnce({email: "luigi.red@email.com", _id: "647edb912de5a0e0e1702caf"}) //OK   
-    Group.findOne.mockResolvedValueOnce(actualGroup_1)
+    Group.updateOne.mockResolvedValueOnce(true) //OK
     User.findOne.mockResolvedValueOnce({email: "pietro.blue@email.com", _id:"647edba4b9ae4baa5e292dec"}) 
-    Group.findOne.mockResolvedValueOnce(actualGroup_2) 
-    Group.findOne.mockResolvedValueOnce(resolvedValue) 
-
-    Group.findOne.mockResolvedValue(true)
+    Group.updateOne.mockResolvedValueOnce(true)
     
    
 
@@ -604,14 +600,13 @@ describe("removeFromGroup", () => {
     expect(mockRes.json).toHaveBeenCalledWith({ data: resolvedValue, refreshedTokenMessage: mockRes.locals.refreshedTokenMessage})
   })
 
-  test.skip("(400) request body does not contain all the necessary attributes", async () => {
-    
+  test("(status 200)[ADMIN] should return group without removed users", async () => {
     const mockReq = {
       params: { 
         name: "Family"},
-      body: {},
-      url:"api/groups/Family/add",
-      cookies: { accessToken:exampleUserAccToken, refreshToken:exampleUserRefToken } //test@test.com user cookies 
+      body: {emails: ["luigi.red@email.com", "pietro.blue@email.com"]},
+      url:"api/groups/Family/pull",
+     
     }
     const mockRes = {
       status: jest.fn().mockReturnThis(),
@@ -620,29 +615,76 @@ describe("removeFromGroup", () => {
         refreshedTokenMessage: "expired token"
       }
     }
-    const actualGroup = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"}]}
-    Group.findOne.mockResolvedValueOnce(actualGroup)
-    verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
-
-    await addToGroup(mockReq, mockRes)
     
+    //Acutal group
+    const actualGroup = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"},{email: "luigi.red@email.com", _id: "647edb912de5a0e0e1702caf"},{email: "pietro.blue@email.com", _id:"647edba4b9ae4baa5e292dec"}]}
+    
+    //Final group with add of luigi and mario 
+    const resolvedValue = {group: {name: "Family", members: [{email: "test@test.com"}]}, membersNotFound: [], notInGroup: []}
+
+    Group.findOne.mockResolvedValueOnce(actualGroup) //OK
+    verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" }) //OK
+    verifyEmail.mockReturnValue({ verified: true, cause: "Verified" })
+    User.findOne.mockResolvedValueOnce({email: "luigi.red@email.com", _id: "647edb912de5a0e0e1702caf"}) //OK   
+    Group.updateOne.mockResolvedValueOnce(true) //OK
+    User.findOne.mockResolvedValueOnce({email: "pietro.blue@email.com", _id:"647edba4b9ae4baa5e292dec"}) 
+    Group.updateOne.mockResolvedValueOnce(true)
+    
+   
+
+    const response = await removeFromGroup(mockReq, mockRes)
+    console.log(response)
+    
+    expect(mockRes.status).toHaveBeenCalledWith(200)
+    expect(mockRes.json).toHaveBeenCalledWith({ data: resolvedValue, refreshedTokenMessage: mockRes.locals.refreshedTokenMessage})
+  })
+
+  test("(400) request body does not contain all the necessary attributes", async () => {
+    
+    const mockReq = {
+      params: { 
+        name: "Family"},
+      body: {},
+      url:"api/groups/Family/remove",
+     
+    }
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+      locals: {
+        refreshedTokenMessage: "expired token"
+      }
+    }
+    
+    //Acutal group
+    const actualGroup = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"},{email: "luigi.red@email.com", _id: "647edb912de5a0e0e1702caf"},{email: "pietro.blue@email.com", _id:"647edba4b9ae4baa5e292dec"}]}
+    
+    //Final group with add of luigi and mario 
+    const resolvedValue = {group: {name: "Family", members: [{email: "test@test.com"}]}, membersNotFound: [], notInGroup: []}
+
+    Group.findOne.mockResolvedValueOnce(actualGroup) //OK
+    verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" }) //OK
+       
+   
+
+    const response = await removeFromGroup(mockReq, mockRes)
+    console.log(response)
     
     expect(mockRes.status).toHaveBeenCalledWith(400)
-    
     expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({
       error: expect.any(String)
   }))
-
+    
   })
 
-  test.skip("(400) group name passed as a route parameter does not represent a group in the database", async () => {
+  test("(400) group name passed as a route parameter does not represent a group in the database", async () => {
     
     const mockReq = {
       params: { 
         name: "GroupNotInDB"},
       body: {},
-      url:"api/groups/GroupNotInDB/add",
-      cookies: { accessToken:exampleUserAccToken, refreshToken:exampleUserRefToken } //test@test.com user cookies 
+      url:"api/groups/Family/remove",
+     
     }
     const mockRes = {
       status: jest.fn().mockReturnThis(),
@@ -651,60 +693,36 @@ describe("removeFromGroup", () => {
         refreshedTokenMessage: "expired token"
       }
     }
-    const actualGroup = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"}]}
-    Group.findOne.mockResolvedValueOnce(actualGroup)
-    verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
-
-    await addToGroup(mockReq, mockRes)
     
+    //Acutal group
+    const actualGroup = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"},{email: "luigi.red@email.com", _id: "647edb912de5a0e0e1702caf"},{email: "pietro.blue@email.com", _id:"647edba4b9ae4baa5e292dec"}]}
+    
+    //Final group with add of luigi and mario 
+    const resolvedValue = {group: {name: "Family", members: [{email: "test@test.com"}]}, membersNotFound: [], notInGroup: []}
+
+    Group.findOne.mockResolvedValueOnce(actualGroup) 
+    verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" }) //OK
+       
+   
+
+    const response = await removeFromGroup(mockReq, mockRes)
+    console.log(response)
     
     expect(mockRes.status).toHaveBeenCalledWith(400)
-    
     expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({
       error: expect.any(String)
   }))
-
+    
   })
 
-  test.skip("(400) if all the provided emails represent users that are already in a group", async () => {
-    
-    const mockReq = {
-      params: { 
-        name: "Family"},
-      body: {emails: ["test@test.com"]},
-      url:"api/groups/Family/add",
-      cookies: { accessToken:exampleUserAccToken, refreshToken:exampleUserRefToken } //test@test.com user cookies 
-    }
-    const mockRes = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-      locals: {
-        refreshedTokenMessage: "expired token"
-      }
-    }
-    const actualGroup = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"}]}
-    Group.findOne.mockResolvedValueOnce(actualGroup)
-    verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
-
-    await addToGroup(mockReq, mockRes)
-    
-    
-    expect(mockRes.status).toHaveBeenCalledWith(400)
-    
-    expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({
-      error: expect.any(String)
-  }))
-
-  })
-
-  test.skip("(400) if all the provided emails represent users that do not exist in the database", async () => {
+  test("(400) all the provided emails represent users that do not exist in the database", async () => {
     
     const mockReq = {
       params: { 
         name: "Family"},
       body: {emails: ["notExisting@test.com"]},
-      url:"api/groups/Family/add",
-      cookies: { accessToken:exampleUserAccToken, refreshToken:exampleUserRefToken } //test@test.com user cookies 
+      url:"api/groups/Family/remove",
+     
     }
     const mockRes = {
       status: jest.fn().mockReturnThis(),
@@ -713,29 +731,36 @@ describe("removeFromGroup", () => {
         refreshedTokenMessage: "expired token"
       }
     }
-    const actualGroup = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"}]}
-    Group.findOne.mockResolvedValueOnce(actualGroup)
-    verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
-
-    await addToGroup(mockReq, mockRes)
     
+    //Acutal group
+    const actualGroup = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"},{email: "luigi.red@email.com", _id: "647edb912de5a0e0e1702caf"},{email: "pietro.blue@email.com", _id:"647edba4b9ae4baa5e292dec"}]}
+    
+    //Final group with add of luigi and mario 
+    const resolvedValue = {group: {name: "Family", members: [{email: "test@test.com"}]}, membersNotFound: [], notInGroup: []}
+
+    Group.findOne.mockResolvedValueOnce(actualGroup) 
+    verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" }) //OK
+       
+   
+
+    const response = await removeFromGroup(mockReq, mockRes)
+    console.log(response)
     
     expect(mockRes.status).toHaveBeenCalledWith(400)
-    
     expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({
       error: expect.any(String)
   }))
-
+    
   })
-
-  test.skip("(400) if all the provided emails represent users that do not exist in the database", async () => {
+  
+  test("(400) at least one of the emails is not in a valid email format", async () => {
     
     const mockReq = {
       params: { 
         name: "Family"},
-      body: {emails: ["emailIncorrectFormat"]},
-      url:"api/groups/Family/add",
-      cookies: { accessToken:exampleUserAccToken, refreshToken:exampleUserRefToken } //test@test.com user cookies 
+      body: {emails: ["notExisting"]},
+      url:"api/groups/Family/remove",
+     
     }
     const mockRes = {
       status: jest.fn().mockReturnThis(),
@@ -744,29 +769,36 @@ describe("removeFromGroup", () => {
         refreshedTokenMessage: "expired token"
       }
     }
-    const actualGroup = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"}]}
-    Group.findOne.mockResolvedValueOnce(actualGroup)
-    verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
-
-    await addToGroup(mockReq, mockRes)
     
+    //Acutal group
+    const actualGroup = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"},{email: "luigi.red@email.com", _id: "647edb912de5a0e0e1702caf"},{email: "pietro.blue@email.com", _id:"647edba4b9ae4baa5e292dec"}]}
+    
+    //Final group with add of luigi and mario 
+    const resolvedValue = {group: {name: "Family", members: [{email: "test@test.com"}]}, membersNotFound: [], notInGroup: []}
+
+    Group.findOne.mockResolvedValueOnce(actualGroup) 
+    verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" }) //OK
+       
+   
+
+    const response = await removeFromGroup(mockReq, mockRes)
+    console.log(response)
     
     expect(mockRes.status).toHaveBeenCalledWith(400)
-    
     expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({
       error: expect.any(String)
   }))
-
+    
   })
 
-  test.skip("(400) at least one of the member emails is an empty string", async () => {
+  test("(400) at least one of the emails is an empty string", async () => {
     
     const mockReq = {
       params: { 
         name: "Family"},
-      body: {emails: ["test@test.com", " "]},
-      url:"api/groups/Family/add",
-      cookies: { accessToken:exampleUserAccToken, refreshToken:exampleUserRefToken } //test@test.com user cookies 
+      body: {emails: [" "]},
+      url:"api/groups/Family/remove",
+     
     }
     const mockRes = {
       status: jest.fn().mockReturnThis(),
@@ -775,30 +807,36 @@ describe("removeFromGroup", () => {
         refreshedTokenMessage: "expired token"
       }
     }
-    const actualGroup = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"}]}
-    Group.findOne.mockResolvedValueOnce(actualGroup)
-    verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" })
-
-    await addToGroup(mockReq, mockRes)
     
+    //Acutal group
+    const actualGroup = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"},{email: "luigi.red@email.com", _id: "647edb912de5a0e0e1702caf"},{email: "pietro.blue@email.com", _id:"647edba4b9ae4baa5e292dec"}]}
+    
+    //Final group with add of luigi and mario 
+    const resolvedValue = {group: {name: "Family", members: [{email: "test@test.com"}]}, membersNotFound: [], notInGroup: []}
+
+    Group.findOne.mockResolvedValueOnce(actualGroup) 
+    verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" }) //OK
+       
+   
+
+    const response = await removeFromGroup(mockReq, mockRes)
+    console.log(response)
     
     expect(mockRes.status).toHaveBeenCalledWith(400)
-    
     expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({
       error: expect.any(String)
   }))
-
+    
   })
 
-  test.skip("(401) at least one of the member emails is an empty string", async () => {
-    const exampleNewUserRefToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im9uZVRlc3RAdGVzdC5jb20iLCJpZCI6IjY0N2Q4NDc1NjJlMmVmODk1MzdlY2MzOCIsInVzZXJuYW1lIjoib25lVGVzdCIsInJvbGUiOiJSZWd1bGFyIiwiaWF0IjoxNjg1OTQ3NTIyLCJleHAiOjE2ODY1NTIzMjJ9.0dD5GNJNUjAQn9oidvLs1DWOmiGZQ-r5eMy5NoWHoqY"
-    const exampleNewUserAccToken = exampleNewUserRefToken
+  test("(400) the group contains only one member before deleting any user", async () => {
+    
     const mockReq = {
       params: { 
         name: "Family"},
-      body: {emails: ["test@test.com", " "]},
-      url:"api/groups/Family/add",
-      cookies: { accessToken:exampleNewUserAccToken, refreshToken:exampleNewUserRefToken } //test@test.com user cookies 
+      body: {emails: ["test@test.com"]},
+      url:"api/groups/Family/remove",
+     
     }
     const mockRes = {
       status: jest.fn().mockReturnThis(),
@@ -807,19 +845,102 @@ describe("removeFromGroup", () => {
         refreshedTokenMessage: "expired token"
       }
     }
-    const actualGroup = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"}]}
-    Group.findOne.mockResolvedValueOnce(actualGroup)
-    verifyAuth.mockReturnValue({ authorized: false, cause: "Unauthorized" })
-
-    await addToGroup(mockReq, mockRes)
     
+    //Acutal group
+    const actualGroup = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"}]}
+    
+    //Final group with add of luigi and mario 
+    const resolvedValue = {group: {name: "Family", members: [{email: "test@test.com"}]}, membersNotFound: [], notInGroup: []}
+
+    Group.findOne.mockResolvedValueOnce(actualGroup) 
+    verifyAuth.mockReturnValue({ authorized: true, cause: "Authorized" }) //OK
+       
+   
+
+    const response = await removeFromGroup(mockReq, mockRes)
+    console.log(response)
+    
+    expect(mockRes.status).toHaveBeenCalledWith(400)
+    expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({
+      error: expect.any(String)
+  }))
+    
+  })
+
+  test("(401) called by an authenticated user who is not part of the group (authType = Group)", async () => {
+    
+    const mockReq = {
+      params: { 
+        name: "Family"},
+      body: {emails: ["test@test.com"]},
+      url:"api/groups/Family/remove",
+     
+    }
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+      locals: {
+        refreshedTokenMessage: "expired token"
+      }
+    }
+    
+    //Acutal group
+    const actualGroup = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"},{email: "luigi.red@email.com", _id: "647edb912de5a0e0e1702caf"},{email: "pietro.blue@email.com", _id:"647edba4b9ae4baa5e292dec"}]}
+    
+    //Final group with add of luigi and mario 
+    const resolvedValue = {group: {name: "Family", members: [{email: "test@test.com"}]}, membersNotFound: [], notInGroup: []}
+
+    Group.findOne.mockResolvedValueOnce(actualGroup) 
+    verifyAuth.mockReturnValue({ authorized: false, cause: "Unauthorized" }) //OK
+       
+   
+
+    const response = await removeFromGroup(mockReq, mockRes)
+    console.log(response)
     
     expect(mockRes.status).toHaveBeenCalledWith(401)
-    
     expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({
       error: expect.any(String)
   }))
+    
+  })
 
+  test("(401) called by an authenticated user who is not admin (authType = Admin)", async () => {
+    
+    const mockReq = {
+      params: { 
+        name: "Family"},
+      body: {emails: ["test@test.com"]},
+      url:"api/groups/Family/pull",
+     
+    }
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+      locals: {
+        refreshedTokenMessage: "expired token"
+      }
+    }
+    
+    //Acutal group
+    const actualGroup = {name: "Family", members: [{email: "test@test.com", user: "6478a58637efc4aff2ee5ee3"},{email: "luigi.red@email.com", _id: "647edb912de5a0e0e1702caf"},{email: "pietro.blue@email.com", _id:"647edba4b9ae4baa5e292dec"}]}
+    
+    //Final group with add of luigi and mario 
+    const resolvedValue = {group: {name: "Family", members: [{email: "test@test.com"}]}, membersNotFound: [], notInGroup: []}
+
+    Group.findOne.mockResolvedValueOnce(actualGroup) 
+    verifyAuth.mockReturnValue({ authorized: false, cause: "Unauthorized" }) //OK
+       
+   
+
+    const response = await removeFromGroup(mockReq, mockRes)
+    console.log(response)
+    
+    expect(mockRes.status).toHaveBeenCalledWith(401)
+    expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({
+      error: expect.any(String)
+  }))
+    
   })
 
  
